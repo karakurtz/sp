b28ef8b9309ede266ef6d135eea36b9b9032d6c4
Bohdan <tsybulenko.bohdan@lll.kpi.ua>
2023-11-24 00:20:45 +0200

    Add laboratory work 2

---
+++ lab2/calculator.h
@@ -0,0 +1,16 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator
+{
+    public:
+<<<<<<< HEAD
+        int Add (double, double);
+        int Sub (double, double);
+=======
+        int Mul (double, double);
+		int Mod (int, int);
+>>>>>>> e0ea21b (add a multiplication operation)
+};
+
+#endif//CALCULATOR_H
